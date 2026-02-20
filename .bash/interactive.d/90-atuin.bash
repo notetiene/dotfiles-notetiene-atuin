ea963bd32e077cde598cc8b7f66ebe1feff0932b
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2026-02-20 17:59:48 -0500

    feat: Add incognito mode in bash and zsh

--- .bash/interactive.d/90-atuin.bash
+++ .bash/interactive.d/90-atuin.bash
@@ -6,3 +6,24 @@
 [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
 
 eval "$(atuin init bash)"
+
+atuin_incognito () {
+  if [[ $1 = disable ]] || [[ $1 == d ]]
+  then
+    # disable incognito
+    if [[ -n "${BLE_VERSION-}" ]]; then
+      echo "no idea what ble is (no incognito mode available here)"
+    else
+      precmd_functions+=(_atuin_precmd)
+      preexec_functions+=(_atuin_preexec)
+    fi
+  else
+    # enable incognito
+    if [[ -n "${BLE_VERSION-}" ]]; then
+      echo "no idea what ble is (no incognito mode available here)"
+    else
+      precmd_functions=("${precmd_functions[@]/_atuin_precmd}")
+      preexec_functions=("${preexec_functions[@]/_atuin_preexec}")
+    fi
+  fi
+}
--- .zsh/interactive.d/90-atuin.zsh
+++ .zsh/interactive.d/90-atuin.zsh
@@ -4,3 +4,14 @@
 # /bin/echo -e '\033[1;32mIn .zsh/interactive.d/90-atuin.zsh\033[0m'
 
 eval "$(atuin init zsh)"
+
+atuin_incognito () {
+    if [[ $1 = disable ]] || [[ $1 == d ]]
+    then
+        add-zsh-hook precmd _atuin_precmd
+        add-zsh-hook preexec _atuin_preexec
+    else
+        add-zsh-hook -d precmd _atuin_precmd
+        add-zsh-hook -d preexec _atuin_preexec
+    fi
+}
